753ff4d490a87ad1f205a2a748e80a29bb89dbfe
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-02-02 01:28:22 +0100

    Add some more basic structure~

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <div>Hello Void</div>;
+  return <div>Products for The Today of Tomorrow</div>;
 }
---
+++ app/products/page.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: {
+    default: 'Products',
+  },
+  description: 'Explore our selection of distinguished products',
+};
+
+export default function ProductPage() {
+  return <div>This is my Product page. This is really cool!</div>;
+}
