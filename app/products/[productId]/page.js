42c2a05468a2903bac6dda3a05b98f5c7154181e
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-02-04 18:33:40 +0100

    Add single product content and basic footer formatting

--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -1,8 +1,23 @@
+import Image from 'next/image';
 import { getProduct } from '../../../database/products';
 
 export default function ProductPage(props) {
   const singleProduct = getProduct(Number(props.params.productId));
   // console.log('Check: ', getProduct(props.params.productId));
   console.log('Check: ', singleProduct);
-  return <div>Singe animal page</div>;
+  return (
+    <div>
+      Single animal page
+      <h1>{singleProduct.name}</h1>
+      <Image
+        src={`/images/${singleProduct.name.toLowerCase()}.png`}
+        alt={singleProduct.name}
+        width={300}
+        height={300}
+      />
+      <h2>{singleProduct.subHeader}</h2>
+      <br />
+      {singleProduct.shortText}
+    </div>
+  );
 }
