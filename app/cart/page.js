d74083a5e12bba640d93ccf950d49bda92849c48
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-02-12 14:16:37 +0100

    Add stringified cookie to cart page

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,3 +1,7 @@
+import { cookies } from 'next/headers';
+import { getProductsInsecure } from '../../database/products';
+import { getCookie } from '../../util/cookies.js';
+
 export const metadata = {
   title: {
     default: 'Portfolio Cart',
@@ -5,12 +9,21 @@ export const metadata = {
   description: 'The portfolio cart contains the products of choice',
 };
 
+const cartItems = JSON.parse(getCookie('itemAmounts'));
+
 export default function CartPage() {
   return (
     <>
       <h1>This is the Cart Page.</h1>
       <br />
       <div>Admire its glorious emptiness.</div>
+      {cartItems.map((cartItem) => {
+        return (
+          <div key={`items-${cartItem.id}`}>
+            <div>{JSON.stringify(cartItem)}</div>
+          </div>
+        );
+      })}
     </>
   );
 }
